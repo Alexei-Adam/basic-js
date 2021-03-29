a6c36e330786585a69e0fe26e38b009ec995f8b3
Alexei-Adam <78561804+Alexei-Adam@users.noreply.github.com>
2021-03-30 01:58:18 +0300

    fear: cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(backyard) {
+  if(backyard.length === 0) return 0;
+  let arr = backyard[0];
+  for(let i; i < backyard.length;i++){
+    arr = arr.concat(backyard[i]);
+  }
+  let count = 0;
+  for(let a=0;a<arr.length;a++){
+    if(arr[a] === '^^') count++;
+  }
+  return count;
 };
