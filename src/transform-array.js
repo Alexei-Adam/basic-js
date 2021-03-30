5038e8102af5851204f709d611e590e1b2667e36
Alexei-Adam <78561804+Alexei-Adam@users.noreply.github.com>
2021-03-31 01:20:58 +0300

    feat: transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,40 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+    let k=0;
+    let ind =0;
+   for(let i=0;i<arr.length;i++){
+     if(arr[i]=='--discard-next'||
+     arr[i]==`--discard-prev`||
+     arr[i]==`--double-next`||
+     arr[i]==`--double-prev`)
+     {k=arr[i];ind = i}
+   }
+   let newarr= [];
+   let ch = 0;
+   
+   if(k=='--discard-next'){
+       alert('m')    
+     for(let i=0;i<arr.length;i++){
+       if(arr[i]!='--discard-next'&&i!=(ind+1)){newarr[ch]=arr[i];ch++}
+     }
+   }
+   else if(k==`--discard-prev`){
+    for(let i=0;i<arr.length;i++){
+     if(arr[i]!=`--discard-prev`&&i!=ind-1){newarr[ch]=arr[i];ch++}
+    }
+    }
+   else if(k==`--double-next`){
+    for(let i=0;i<arr.length;i++){
+     if(arr[i]!=`--double-next`){newarr[i]=arr[i]}else newarr[i]=arr[i+1];
+    }
+    }
+   else if(k==`--double-prev`){
+      
+    for(let i=0;i<arr.length;i++){
+     if(arr[i]!=`--double-prev`){newarr[i]=arr[i]}else newarr[i]=arr[i-1];
+    }
+    }
+
+   return newarr;
+  };
